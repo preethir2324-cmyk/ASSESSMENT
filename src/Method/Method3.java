27c3fe9bc134bd2219382751fe0aaf69f3c464ec
preethir2324-cmyk <preethir2324@gmail.com>
2026-02-20 16:35:33 +0530

    method

---
+++ src/Method/Method3.java
@@ -0,0 +1,14 @@
+package Method;
+
+public class Method3 {
+    static int add(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        String result=String.format("Sum is %d:", add(10,20));
+        System.out.println(result);
+
+    }
+}
+
